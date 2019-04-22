46059319a13d57cda03d68c3f18b8f25542b3bc8
Dima <dima.kalanchyk98@gmail.com>
2019-04-22 19:15:45 +0200

    Это наш первый комит

---
+++ script2.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9)
+
+let something = 10 ;
+console.log(something)
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.pmj', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0])
+
+alert("Hello World!");
+
+confirm("Are you here?")
+
